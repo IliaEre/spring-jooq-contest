37b09c233f1a6fc8474d4d9989dd5555531d056b
Ilia Ivankin <ilia_ivankin@epam.com>
2021-10-21 19:47:22 +0300

    added based jooq generator

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,17 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import com.rohanprabhu.gradle.plugins.kdjooq.*
 
 plugins {
     id("org.springframework.boot") version "2.5.6"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
     kotlin("jvm") version "1.5.31"
     kotlin("plugin.spring") version "1.5.31"
+    // follow here: https://github.com/rohanprabhu/kotlin-dsl-gradle-jooq-plugin
+    id("com.rohanprabhu.kotlin-dsl-jooq") version "0.4.6"
 }
 
 group = "com.epam.stock"
-version = "0.0.1-SNAPSHOT"
+version = "0.0.1"
 java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
@@ -22,6 +25,8 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    // added jooq generator
+    jooqGeneratorRuntime("org.postgresql:postgresql:42.1.4")
     runtimeOnly("org.postgresql:postgresql")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
@@ -35,4 +40,31 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+// we should create a task like:
+jooqGenerator {
+    configuration("primary", project.java.sourceSets.getByName("main")) {
+        configuration = jooqCodegenConfiguration {
+            jdbc {
+                username = "postgres"
+                password = "pwd"
+                driver = "org.postgresql.Driver"
+                url = "jdbc:postgresql://localhost:5432/postgres"
+            }
+
+            generator {
+                target {
+                    packageName = "com.epam.jooq"
+                    directory = "${project.buildDir}/generated/jooq/primary"
+                }
+
+                database {
+                    name = "org.jooq.meta.postgres.PostgresDatabase"
+                    inputSchema = "public"
+                    includes = "product|description"
+                }
+            }
+        }
+    }
 }
