cecb1f7c5bac6858235c3048b579b4cc00b3f716
Ilia Ivankin <ilia_ivankin@epam.com>
2021-10-21 19:10:13 +0300

    init simple service

---
+++ src/test/kotlin/com/epam/stock/jooqcontest/JooqContestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.epam.stock.jooqcontest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JooqContestApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
